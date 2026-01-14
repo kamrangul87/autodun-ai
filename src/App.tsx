8a7e091775a90e27af4b1663f37a204559ecacb3
kamrangul87 <kamrangul87@gmail.com>
2026-01-14 07:03:05 +0000

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,7 @@ import Contact from "./pages/Contact";
 import Privacy from "./pages/Privacy";
 import Terms from "./pages/Terms";
 import Cookies from "./pages/Cookies";
+import Blog from "./pages/Blog"; // ✅ added
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
@@ -30,6 +31,7 @@ const App = () => (
           <Route path="/data-usage" element={<DataUsage />} />
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
+          <Route path="/blog" element={<Blog />} /> {/* ✅ added */}
           <Route path="/privacy" element={<Privacy />} />
           <Route path="/terms" element={<Terms />} />
           <Route path="/cookies" element={<Cookies />} />
